6c19adc6207b73f07f6b80eba9e67ab9f672cf9b
Polina0709 <melnik.polya@gmail.com>
2025-05-04 18:51:15 +0300

    Initial commit

---
+++ src/main/java/com/flowbank/PaymentApp2Application.java
@@ -0,0 +1,13 @@
+package com.flowbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApp2Application.class, args);
+    }
+
+}
